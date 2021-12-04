5989a40ffc9af5f2cfe925337c146efdac76320d
MarcinDydo <marcin.dydo.stud@pw.edu.pl>
2021-12-04 11:34:16 -0500

    cleanup jakis na kiju

--- ActionManagers/LogManager.py
+++ ActionManagers/LogManager.py
@@ -5,9 +5,7 @@ import HttpClient.HttpClient as http_client
 
 def get_logs_list(host_address):
 
-    command = command_model.Command(action=command_model.get_logs_list,bpf_filter = command_model.default_filter,
-                                    time = command_model.default_time, names = command_model.default_names,
-                                     command = command_model.default_command  )
+    command = command_model.Command(action=command_model.get_logs_list)
     http_client.send_command(host_address,command)
     #TODO rezultat powinno wyświetlić
 
@@ -15,8 +13,7 @@ def get_logs_list(host_address):
 
 def get_logs(host_address,names):
 
-    command = command_model.Command(action=command_model.get_logs, bpf_filter = command_model.default_filter,
-                                    time = command_model.default_time, names = names,
-                                     command = command_model.default_command  )
+    command = command_model.Command(action=command_model.get_logs,
+                                    names = names)
     http_client.send_command(host_address,command)
     #TODO powinno pobrać pliki do jakiegoś folderu lokalnego np Logs
--- ActionManagers/PcapManager.py
+++ ActionManagers/PcapManager.py
@@ -1,37 +1,25 @@
 import Model.Command as command_model
 import HttpClient.HttpClient as http_client
 
-def get_configuration(host_address):
-
-    command = command_model.Command(action=command_model.get_configuration,bpf_filter = command_model.default_filter,
-                                    time = command_model.default_time, names = command_model.default_names,
-                                     command = command_model.default_command  )
-    http_client.send_command(host_address,command)
-    #TODO rezultat powinno wyświetlić
-
 
 def capture_traffic(host_address, bpf_filter, time):
-    command = command_model.Command(action=command_model.capture_traffic,bpf_filter = bpf_filter,
-                                    time = time, names = command_model.default_names,
-                                     command = command_model.default_command  )
+    command = command_model.Command(action=command_model.capture_traffic,
+                                    parameters = bpf_filter,
+                                    time = time)
     http_client.send_command(host_address,command)
 
 
 def get_captures_list(host_address):
 
-    command = command_model.Command(action=command_model.get_captures_list,bpf_filter = command_model.default_filter,
-                                    time = command_model.default_time, names = command_model.default_names,
-                                     command = command_model.default_command  )
+    command = command_model.Command(action=command_model.get_captures_list)
     http_client.send_command(host_address,command)
     #TODO rezultat powinno wyświetlić
 
 
-
 def get_captures(host_address,names):
 
-    command = command_model.Command(action=command_model.get_captures,bpf_filter = command_model.default_filter,
-                                    time = command_model.default_time, names = names,
-                                     command = command_model.default_command  )
+    command = command_model.Command(action=command_model.get_captures, 
+                                    names = names)
     http_client.send_command(host_address,command)
     #TODO powinno pobrać pliki do jakiegoś folderu lokalnego np Captures
 

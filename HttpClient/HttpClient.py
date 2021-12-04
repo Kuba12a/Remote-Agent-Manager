167ff4045ee285f2c0691327fa913f0c66e63d64
kalus <kaluski1212@wp.pl>
2021-12-04 19:19:00 +0100

    Add FileAPI.py & add get_filenames(URL) in HttpClient.py

--- HttpClient/HttpClient.py
+++ HttpClient/HttpClient.py
@@ -1,7 +1,6 @@
 import requests
 import Model.Command as Command
 import json
-import datetime
 
 
 def send_command(URL, command : Command):
@@ -18,3 +17,19 @@ def send_command(URL, command : Command):
     # extracting data in json format
     
     return status_code
+
+
+
+def get_filenames(URL):
+    status_code = None
+    try:
+        response = requests.get(url=URL)
+        status_code = response.status_code
+        result = response.json()
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
+    # extracting data in json format
+
+    return result

3e3f0d3e020bbf798fa907319c30dc5ae4f99d3e
MarcinDydo <01149354@pw.edu.pl>
2021-12-19 16:17:53 -0500

    minor poprawki

--- ActionManagers/ExecuteCommandManager.py
+++ ActionManagers/ExecuteCommandManager.py
@@ -1,9 +1,11 @@
 import Model.Command as command_model
 import HttpClient.HttpClient as http_client
+from pprint import pprint
 
 
 def execute_command(host_address,execution_command):
     command = command_model.Command(parameters = execution_command, action = command_model.execute_command)
 
     url = host_address
-    http_client.send_command(url, command)
+    result = http_client.send_command(url, command)
+    print(result)
--- HttpClient/HttpClient.py
+++ HttpClient/HttpClient.py
@@ -21,7 +21,7 @@ def send_command(URL, command : Command):
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(message)
-    formatted = str(response.json()).replace("\n","\n")
+    formatted = str(response.json()).replace("\\n","\n")
     return formatted
 
 
